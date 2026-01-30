6e66d6af1dbb948f5a253b2b0c38bdf9cd533b26
Anton Kireev <AGKireev@users.noreply.github.com>
2026-01-30 19:05:20 +0100

    TikTok pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,7 @@ export default function RootLayout({
 }>) {
   const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
   const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID
+  const tiktokPixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID
   const isProduction = process.env.NODE_ENV === 'production'
   
   return (
@@ -115,6 +116,18 @@ export default function RootLayout({
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '${metaPixelId}');
               fbq('track', 'PageView');
+            `}
+          </Script>
+        )}
+        {/* TikTok Pixel - Only load in production to avoid polluting analytics */}
+        {isProduction && tiktokPixelId && (
+          <Script id="tiktok-pixel" strategy="beforeInteractive">
+            {`
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+                ttq.load('${tiktokPixelId}');
+                ttq.page();
+              }(window, document, 'ttq');
             `}
           </Script>
         )}
